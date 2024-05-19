bb022ddd002f3314d841c2ad8d00ef4cc32a3abc
Samolazoff <samolazov.roman.alex@gmail.com>
2024-05-19 22:15:21 +0300

    Add HeroLeft block

--- src/app/components/ui/hero/HeroRight.tsx
+++ src/app/components/ui/hero/HeroRight.tsx
@@ -1,13 +1,34 @@
 import {FC}from 'react';
 
+import Image from 'next/image';
+
 import styles from './Hero.module.scss';
 
 const HeroRight: FC = () => {
     return (
-      <section className={styles.hero}>
-        HeroRight
+      <section className={styles.hero_right}>
+        <div className={styles.hero_right_wrap_img_first}>
+          <Image 
+            src='/sportsman_1.jpg'
+            alt='HERO'
+            priority
+            width={500}
+            height={500}
+            className={styles.hero_right_img}
+            />
+        </div>
+        <div className={styles.hero_right_wrap_img_second}>
+          <Image 
+            src='/sportsman_2.jpg'
+            alt='HERO'
+            priority
+            width={500}
+            height={500}
+            className={styles.hero_right_img}
+            />
+        </div>
       </section>
     );
 };
 
-export default HeroRight;
+export default HeroRight; 
