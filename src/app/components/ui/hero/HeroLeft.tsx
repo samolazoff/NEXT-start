cecd2ff38a5bb6745b2802e9dce9b037df1b12ec
unknown <samolazov.roman.alex@gmail.com>
2024-05-19 10:16:37 +0300

    Add HeroLeft block

--- src/app/components/ui/hero/HeroLeft.tsx
+++ src/app/components/ui/hero/HeroLeft.tsx
@@ -4,8 +4,13 @@ import styles from './Hero.module.scss';
 
 const HeroLeft: FC = () => {
     return (
-        <section className={styles.hero}>
-          Heroleft
+        <section className={styles.hero_left}>
+          <h2 className={styles.hero_left_title}>
+            Top Fashion for a top price
+          </h2>
+          <p className={styles.hero_left_txt}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cum velit nulla, aliquam dolorum tempore facere ducimus accusantium nemo! Minima dolorem alias error aspernatur!
+          </p>
         </section>
     );
 };
